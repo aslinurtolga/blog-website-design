3ca5ee46e4a02b07781ea3f833feb6a2335a8887
AslinurTolga <aslinurtolga@gmail.com>
2023-01-03 01:47:36 +0300

    feat: single post components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
-import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Single/>
+    <Write/>
     </>
   );
 }
