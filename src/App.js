7b45d9ee32bb79438f7f6fc353f2f4f8cb97cee5
AslinurTolga <aslinurtolga@gmail.com>
2023-01-03 02:55:27 +0300

    feat: login is created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
-import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Settings/>
+    <Login/>
     </>
   );
 }
